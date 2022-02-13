294a40cfddc9e52c6e277f446cbda9b9e64290fd
andreaalmanza <aalmanza5@csub.edu>
2022-02-12 20:30:48 -0800

    Started a2

--- Inventory/src/a1/aalmanza5/Main.java
+++ Inventory/src/a1/aalmanza5/Main.java
@@ -6,81 +6,38 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    private static final Random ran = new Random();
+
+    private static final FoodItems[] foodItems = FoodItems.values();
+    private static final Tools[] tools = Tools.values();
+    private static final ToolUses[] toolUses = ToolUses.values();
+    private static final OutfitItems[] outfitItems = OutfitItems.values();
+    private static final CoolFit[] coolFits = CoolFit.values();
+    private static final CarItems[] carItems = CarItems.values();
 
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        Random ran = new Random();
         List<Item> items = new ArrayList<>();
-        FoodItems[] foodItems = FoodItems.values();
-        Tools[] tools = Tools.values();
-        ToolUses[] toolUses = ToolUses.values();
-        OutfitItems[] outfitItems = OutfitItems.values();
-        CoolFit[] coolFits = CoolFit.values();
-        CarItems[] carItems = CarItems.values();
+
         System.out.print("HOw many items do you want: ");
 	    int itemCnt = Integer.parseInt(scan.nextLine());
 
         for(int i = 0; i < itemCnt; i++){
-            int type = ran.nextInt(2);
+            int type = ran.nextInt(4);
             switch (type) {
-                case 0 -> {
-                    int foodIndex = ran.nextInt(foodItems.length);
-                    String foodName = foodItems[foodIndex].toString();
-                    float foodPrice = ran.nextFloat(10);
-                    int foodQty = ran.nextInt(30);
-                    int foodUses = ran.nextInt(6);
-                    float healthGain = ran.nextFloat(20);
-                    Food tmpFood = new Food(foodName, foodPrice, foodQty, foodUses, healthGain);
-                    //System.out.println(c);
-                    items.add(tmpFood);
-                }
-                case 1 -> {
-                    int toolIndex = ran.nextInt(tools.length);
-                    String toolName = tools[toolIndex].toString();
-                    float toolPrice = ran.nextFloat(200);
-                    int toolQty = ran.nextInt(15);
-                    String use = toolUses[toolIndex].toString();
-                    Tool tmpTool = new Tool(toolName, toolPrice, toolQty, use);
-                    //System.out.println(tmp);
-                    items.add(tmpTool);
+                case 0 -> items.add(genFood());
 
-                    int outfitIndex = ran.nextInt(outfitItems.length);
-                    String fitName = outfitItems[outfitIndex].toString();
-                    float fitPrice = ran.nextFloat(100);
-                    int fitQty = ran.nextInt(200);
-                    String wears = coolFits[outfitIndex].toString();
-                    Outfit tmpOutfit = new Outfit(fitName, fitPrice, fitQty, wears);
-                    items.add(tmpOutfit);
+                case 1 -> items.add(genTool());
+                    
 
-                    int carIndex = ran.nextInt(carItems.length);
-                    String carName = carItems[carIndex].toString();
-                    float carPrice = ran.nextFloat(10000);
-                    int carQty = ran.nextInt(30);
-                    float fast = ran.nextFloat(500);
-                    Cars tmpCars = new Cars(carName, carPrice, carQty, fast);
-                    //System.out.println(c);
-                    items.add(tmpCars);
-                }
-                case 2 -> {
-                    int outfitIndex = ran.nextInt(outfitItems.length);
-                    String fitName = outfitItems[outfitIndex].toString();
-                    float fitPrice = ran.nextFloat(100);
-                    int fitQty = ran.nextInt(200);
-                    String wears = coolFits[outfitIndex].toString();
-                    Outfit tmpOutfit = new Outfit(fitName, fitPrice, fitQty, wears);
-                    items.add(tmpOutfit);
-                }
-                case 3 -> {
-                    int carIndex = ran.nextInt(carItems.length);
-                    String carName = carItems[carIndex].toString();
-                    float carPrice = ran.nextFloat(10000);
-                    int carQty = ran.nextInt(30);
-                    float fast = ran.nextFloat(500);
-                    Cars tmpCars = new Cars(carName, carPrice, carQty, fast);
-                    //System.out.println(c);
-                    items.add(tmpCars);
-                }
+
+                case 2 -> items.add(genOutfit());
+
+
+
+                case 3 -> items.add(genCars());
+
             }
 
         }
@@ -91,6 +48,51 @@ public class Main {
 
 
 
+
+    }
+
+
+
+
+    public static Food genFood() {
+        int foodIndex = ran.nextInt(foodItems.length);
+        String foodName = foodItems[foodIndex].toString();
+        float foodPrice = ran.nextFloat(10);
+        int foodQty = ran.nextInt(30);
+        int foodUses = ran.nextInt(6);
+        float healthGain = ran.nextFloat(20);
+        return new Food(foodName, foodPrice, foodQty, foodUses, healthGain);
+        //System.out.println(c);
+        //items.add(tmpFood);
+
+    }
+    public static Tool genTool() {
+        int toolIndex = ran.nextInt(tools.length);
+        String toolName = tools[toolIndex].toString();
+        float toolPrice = ran.nextFloat(200);
+        int toolQty = ran.nextInt(15);
+        String use = toolUses[toolIndex].toString();
+        return new Tool(toolName, toolPrice, toolQty, use);
+
+    }
+
+    public static Outfit genOutfit() {
+        int outfitIndex = ran.nextInt(outfitItems.length);
+        String fitName = outfitItems[outfitIndex].toString();
+        float fitPrice = ran.nextFloat(100);
+        int fitQty = ran.nextInt(200);
+        String wears = coolFits[outfitIndex].toString();
+        return new Outfit(fitName, fitPrice, fitQty, wears);
+
+    }
+
+    public static Cars genCars() {
+        int carIndex = ran.nextInt(carItems.length);
+        String carName = carItems[carIndex].toString();
+        float carPrice = ran.nextFloat(10000);
+        int carQty = ran.nextInt(30);
+        float fast = ran.nextFloat(500);
+        return new Cars(carName, carPrice, carQty, fast);
 
     }
 }
---
+++ MultiThreadSort/src/a2/aalmanza5/Main.java
@@ -0,0 +1,12 @@
+package a2.aalmanza5;
+
+import a1.aalmanza5.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Food f = a1.aalmanza5.Main.genFood();
+        System.out.println(f);
+    }
+}
