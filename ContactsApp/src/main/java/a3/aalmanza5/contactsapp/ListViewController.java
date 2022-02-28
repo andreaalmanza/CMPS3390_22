6683e9928e56716ec3f52a66e53f9a3849d1a8f0
andreaalmanza <aalmanza5@csub.edu>
2022-02-27 23:41:47 -0800

    a3 lab done.

--- ContactsApp/src/main/java/a3/aalmanza5/contactsapp/ListViewController.java
+++ ContactsApp/src/main/java/a3/aalmanza5/contactsapp/ListViewController.java
@@ -2,16 +2,20 @@ package a3.aalmanza5.contactsapp;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class ListViewController {
@@ -23,6 +27,12 @@ public class ListViewController {
     GridPane newContact;
     @FXML
     TextField txtFirstName, txtLastName, txtPhone;
+
+    @FXML
+    MenuItem btnThemeDefault, btnThemeBlue, btnThemePink;
+
+    @FXML
+    VBox vboxMain;
 
     ContactComparator comparator = new ContactComparator();
     @FXML
@@ -87,6 +97,23 @@ public class ListViewController {
             txtPhone.setText(selectedContact.getPhone());
             onDeleteContact();
 
+        }
+    }
+
+    @FXML
+    protected void onThemeChange(final ActionEvent event){
+        String defaultCSS = Objects.requireNonNull(getClass().getResource("Default.css")).toString();
+        String blueCSS = Objects.requireNonNull(getClass().getResource("Blue.css")).toString();
+        String pinkCSS = Objects.requireNonNull(getClass().getResource("Pink.css")).toString();
+        vboxMain.getScene().getStylesheets().removeAll(defaultCSS, blueCSS, pinkCSS);
+
+        Object source = event.getSource();
+        if (btnThemeDefault.equals(source)) {
+            vboxMain.getScene().getStylesheets().add(defaultCSS);
+        } else if (btnThemeBlue.equals(source)) {
+            vboxMain.getScene().getStylesheets().add(blueCSS);
+        } else if(btnThemePink.equals(source)){
+            vboxMain.getScene().getStylesheets().add(pinkCSS);
         }
     }
 
