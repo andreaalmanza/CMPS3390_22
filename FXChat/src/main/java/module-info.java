7f94948bf7210c813aa18a8c2924961d44214a1b
andreaalmanza <aalmanza5@csub.edu>
2022-03-20 15:57:42 -0700

    a6 done

---
+++ FXChat/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module a6.aalmanza5.fxchat.fxchat {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens a6.aalmanza5.fxchat.fxchat to javafx.fxml;
+    exports a6.aalmanza5.fxchat.fxchat;
+}
